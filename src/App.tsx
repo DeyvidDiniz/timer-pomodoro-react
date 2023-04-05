155af904ce33cb5e9dc588ab599001b6ec15a1cd
DeyvidDiniz <deyvidatd@hotmail.com>
2023-04-05 14:48:57 -0300

    Configurações de Temas e estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { useState } from 'react'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1>Hello World</h1>
-  )
+  return <h1>Hello World</h1>
 }
-
